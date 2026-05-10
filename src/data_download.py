c6cb54c335d791bc0a12dfb61ef80496c56ae0b5
ziad0ayman <zezokmezo@gmail.com>
2026-05-11 00:52:47 +0300

    Refactor data_download script for portability and reusability.  Moved the dataset URL to an external config.json file and updated the script to use relative path anchoring.

--- src/data_download.py
+++ src/data_download.py
@@ -1,13 +1,20 @@
 import os
 import shutil
+import json
 import kagglehub
 
 # 1. Anchor the destination path to wherever this script is located
 script_dir = os.path.dirname(os.path.abspath(__file__))
 dest_path = os.path.join(script_dir, "..", "data", "raw")
+config_path = os.path.join(script_dir, "..", "config", "config.json") # Path to your new config file
+
+# Read the config file
+with open(config_path, 'r') as f:
+    config = json.load(f)
+dataset_name = config["dataset_link"]
 
 # 2. Download the dataset using kagglehub
-source_path = kagglehub.dataset_download('geadalfa/cracked-non-cracked-surface-datasets')
+source_path = kagglehub.dataset_download(dataset_name)
 print(f"Dataset temporarily downloaded to cache: {source_path}")
 
 # 3. Check for nested root folders
